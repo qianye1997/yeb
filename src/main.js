77cf2db01e4c9e851d7fedd4878560297bb2e3a7
qianye1997 <duanbf97@163.com>
2021-05-16 10:53:34 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false;
-// 安装ElementUI
-Vue.use(ElementUI);
+Vue.config.productionTip = false
 
-createApp(App).use(router).mount('#app');
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Login.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -13,14 +16,11 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
+const router = new VueRouter({
   routes
 })
 
